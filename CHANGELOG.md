639903b8d88ce6447e0a893542e3040204544fb0
pengqiang324 <839556387@qq.com>
2023-06-14 19:53:51 +0800

    feat(日志管理): 🚀 changelog custom

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,9 @@
-## 1.2.6 (2023-06-14)
+## <small>1.2.7 (2023-06-14)</small>
+
+* feat: 🚀 测试中 ([fd0a55c](https://github.com/pengqiang324/application-theme-plus/commit/fd0a55c))
+* feat: 特性(视图模块): 🚀 支持中文提交代码 ([f7ca970](https://github.com/pengqiang324/application-theme-plus/commit/f7ca970))
+* feat(commit-rule): 🚀 提交代码规范配置填充(#1) ([ce2f9d3](https://github.com/pengqiang324/application-theme-plus/commit/ce2f9d3))
+* fix(custom1): 🧩 修改程序 ([8cd583c](https://github.com/pengqiang324/application-theme-plus/commit/8cd583c))
 
 
 
--- changelog-option.js
+++ changelog-option.js
@@ -1,6 +1,14 @@
+const compareFunc = require("compare-func");
 module.exports = {
   writerOpts: {
     transform: (commit, context) => {
+      let discard = true;
+      const issues = [];
+
+      commit.notes.forEach((note) => {
+        note.title = "BREAKING CHANGES";
+        discard = false;
+      });
       if (commit.type === "feat") {
         commit.type = "✨ Features | 新功能";
       } else if (commit.type === "fix") {
@@ -9,8 +17,55 @@ module.exports = {
         commit.type = "⚡ Performance Improvements | 性能优化";
       } else if (commit.type === "revert" || commit.revert) {
         commit.type = "⏪ Reverts | 回退";
+      } else if (discard) {
+        return;
       }
-      return;
+
+      if (commit.scope === "*") {
+        commit.scope = "";
+      }
+      if (typeof commit.hash === "string") {
+        commit.hash = commit.hash.substring(0, 7);
+      }
+      if (typeof commit.subject === "string") {
+        let url = context.repository
+          ? `${context.host}/${context.owner}/${context.repository}`
+          : context.repoUrl;
+        if (url) {
+          url = `${url}/issues/`;
+
+          commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
+            issues.push(issue);
+            return `[#${issue}](${url}${issue})`;
+          });
+        }
+        if (context.host) {
+          commit.subject = commit.subject.replace(
+            /\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g,
+            (_, username) => {
+              if (username.includes("/")) {
+                return `@${username}`;
+              }
+
+              return `[@${username}](${context.host}/${username})`;
+            }
+          );
+        }
+      }
+
+      commit.references = commit.references.filter((reference) => {
+        if (issues.indexOf(reference.issue) === -1) {
+          return true;
+        }
+
+        return false;
+      });
+      return commit;
     },
+    groupBy: "type",
+    commitGroupsSort: "title",
+    commitsSort: ["scope", "subject"],
+    noteGroupsSort: "title",
+    notesSort: compareFunc,
   },
 };
