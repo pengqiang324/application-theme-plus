31b48f6c80b76c09a923ade3f347ee638ae06339
pengqiang324 <839556387@qq.com>
2023-06-15 14:02:17 +0800

    feat(ci集成): 🚀 自动化功能

--- theme/components/BaseSetting/index.js
+++ theme/components/BaseSetting/index.js
@@ -1,7 +1,8 @@
-import zBaseSetting from './src/BaseSetting.vue';
- 
+import zBaseSetting from "./src/BaseSetting.vue";
+
 zBaseSetting.install = (Vue) => {
-  Vue.component(zBaseSetting.name,zBaseSetting)
-}
- 
+  console.log(1);
+  Vue.component(zBaseSetting.name, zBaseSetting);
+};
+
 export default zBaseSetting;
