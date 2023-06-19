9631ee5c89873406c175a4dbbc6e028727395fde
pengqiang324 <839556387@qq.com>
2023-06-19 16:52:46 +0800

    feat(业务功能): ✨ 新增 eslint 功能

--- examples/App.vue
+++ examples/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <router-view></router-view>
+    <router-view></router-view>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+    name: 'App'
+};
 </script>
 
 <style>
 body {
-  background: #fafafa;
+    background: #fafafa;
 }
 </style>
