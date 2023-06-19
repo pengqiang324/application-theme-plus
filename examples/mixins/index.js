5ff4c12b3fe2765b20621eb4478eafef24e4c950
pengqiang324 <839556387@qq.com>
2023-06-19 17:53:57 +0800

    fix(业务功能): 🧩 规范代码

--- examples/mixins/index.js
+++ examples/mixins/index.js
@@ -1,20 +1,20 @@
-import { ref, nextTick } from "vue"
+import { ref, nextTick } from 'vue';
 
-export default function() {
-    const tableRef = ref(null)
-    const checkListMixin = ref([])
-    const checkChange = (checkList) => {
-        checkListMixin.value = checkList
+export default function () {
+    const tableRef = ref(null);
+    const checkListMixin = ref([]);
+    const checkChange = checkList => {
+        checkListMixin.value = checkList;
         nextTick(() => {
-            tableRef.value.doLayout()
-        })
-    }
-    const filterLabel = (label) => {
-        return checkListMixin.value.includes(label)
-    }
+            tableRef.value.doLayout();
+        });
+    };
+    const filterLabel = label => {
+        return checkListMixin.value.includes(label);
+    };
     return {
         tableRef,
         checkChange,
         filterLabel
-    }
+    };
 }
