5761bec83e13185ff1348f550901a3ffe0c5057a
pengqiang324 <839556387@qq.com>
2023-06-19 17:56:57 +0800

    fix(业务功能): 🧩 预发布版本功能

--- examples/router/index.js
+++ examples/router/index.js
@@ -1,28 +1,28 @@
-import { createRouter,createWebHistory } from 'vue-router'
- 
+import { createRouter, createWebHistory } from 'vue-router';
+
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue'),
-    children: [
-      {
-        path: '/button',
-        name: 'Button',
-        component: () => import('../views/Button.vue')
-      }
-    ]
-  },
-  {
-    path: '/404',
-    name: '404',
-    component: () => import('../views/404.vue')
-  }
-]
- 
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/Home.vue'),
+        children: [
+            {
+                path: '/button',
+                name: 'Button',
+                component: () => import('../views/Button.vue')
+            }
+        ]
+    },
+    {
+        path: '/404',
+        name: '404',
+        component: () => import('../views/404.vue')
+    }
+];
+
 const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
- 
-export default router
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
