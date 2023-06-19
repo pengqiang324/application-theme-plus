23f36c41b6d3868893beb0c6dfd854bfaa66cbce
pengqiang324 <839556387@qq.com>
2023-06-19 18:17:48 +0800

    fix(业务功能): 🧩 优化

---
+++ my-options.js
@@ -0,0 +1,11 @@
+module.exports = {
+    types: [
+        { type: 'feat', section: '✨ Features | 新功能' },
+        { type: 'fix', section: '🐛 Bug Fixes | Bug 修复' },
+        { type: 'chore', hidden: true },
+        { type: 'docs', hidden: true },
+        { type: 'style', hidden: true },
+        { type: 'refactor', hidden: true },
+        { type: 'perf', hidden: true }
+    ]
+};
